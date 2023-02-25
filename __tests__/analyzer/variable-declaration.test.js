8be97f4883ab08e6047cdd8e093758aa95b3e882
Christopher Ribeiro <christo_campiglia@hotmail.com>
2023-02-25 19:20:47 -0300

    update variable declaration tests
    make sure that references rules get enforced inside if and if else statements

--- __tests__/analyzer/variable-declaration.test.js
+++ __tests__/analyzer/variable-declaration.test.js
@@ -1,6 +1,6 @@
 import { describe, it, expect } from "vitest";
-import Parser from "../../../src/compiler/parser";
-import Analyzer from "../../../src/compiler/analyzer";
+import Parser from "../../src/compiler/parser";
+import Analyzer from "../../src/compiler/analyzer";
 
 describe("Identifier errors", () => {
     it("should throw if variable already declared in program scope", () => {
@@ -16,6 +16,16 @@ describe("Identifier errors", () => {
     it("should throw if param already declared in function scope as variable", () => {
         const parser = new Parser();
         expect(() => Analyzer(parser.parse("function a(a) { let a = 1 }")))
+            .toThrowError(/This identifier has already been declared./);
+    });
+    it("should throw if variable already declared in if scope", () => {
+        const parser = new Parser();
+        expect(() => Analyzer(parser.parse("function a() { if (a) { let a = 0 let a = 1 } }")))
+            .toThrowError(/This identifier has already been declared./);
+    });
+    it("should throw if variable already declared in else if scope", () => {
+        const parser = new Parser();
+        expect(() => Analyzer(parser.parse("function a() { if (a) {} else if (a) { let a = 0 let a = 1 } }")))
             .toThrowError(/This identifier has already been declared./);
     });
 });
